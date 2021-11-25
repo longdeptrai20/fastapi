17726ae7c27675bbfee8d4eab91e61381997f1dc
longdeptrai20 <maiduclong11@gmail.com>
2021-11-26 00:06:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 async def objectdetect(hinhanh: str):
     object_detect = od.read(hinhanh)
     return object_detect
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
 # uvicorn WebService.main:app
