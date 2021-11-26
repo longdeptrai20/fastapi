86f6e754f67438f14a8d5bcf72c7491c835c96fc
longdeptrai20 <maiduclong11@gmail.com>
2021-11-26 15:00:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 import objectdetect as od
 
 app = FastAPI()
+
 @app.post('/objectdetect')
-def objectdetect(hinhanh: str):
-    object_detect = od.read(hinhanh)
+async def objectdetect(images: str):
+    object_detect = od.read(images)
     return object_detect
 
 @app.get("/")
